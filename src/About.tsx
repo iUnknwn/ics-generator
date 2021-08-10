59434e9edd8847e8a83e7d947aed0685b14e161a
iUnknwn <sezimmer@microsoft.com>
2021-08-09 20:09:51 -0700

    Initial Commit.

---
+++ src/About.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+    return (
+        <div style={{marginTop: "3em"}}>
+            <h2>About This Tool</h2>
+            <p>This tool was built to provide a simple, straightforward way to build ICS files,
+               without sending your event information back to some server. This app is written
+               in javascript and runs completely in your browser. 
+            </p>
+            <p>Built with Typescript, Material-UI components, and the ics package.</p>
+        </div>
+    )
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import EventInfo from './CalendarComponents/EventInfo'
+import About from './About'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Simple ICS File Generator</h1>
       </header>
+      <div className="mainContent">
+        <p>Generate an ICS event file, compatible with Google Calendar, iCal, Outlook, and more.</p>
+        <EventInfo />
+        <About />
+      </div>
     </div>
   );
 }
